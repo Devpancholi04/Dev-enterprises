e2465766b3262bac69fe10ee730696e30c8dd9fb
Dev Pancholi <112951112+Devpancholi04@users.noreply.github.com>
2023-09-17 15:36:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// toggle search bar functioning
+
+function toggle(){
+    var x = document.getElementById("toggle");
+        if(x.style.display === "none") {
+            x.style.display = "block";
+        }
+
+        else{
+            x.style.display = "none";
+        }
+}
+
+
+
